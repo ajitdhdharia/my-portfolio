e812e05206786e0a62210caffb427903ca15ac3e
dhdharia <ajitdhdhariaaa@gmail.com>
2021-03-31 09:57:23 -0400

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import Home from "./components/Home";
+import About from "./components/About"
 function App() {
   return (
     <div>
       <Home></Home>
+      <About></About>
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <ul class="navbar-nav mx-auto d-inline-flex flex-row">
+            <li class="nav-item px-2">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item px-2">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item px-2">
+              <a class="nav-link" href="#">Skills</a>
+            </li>
+            <li class="nav-item px-2">
+              <a class="nav-link" href="#">Project</a>
+            </li>
+            <li class="nav-item px-2">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+export default About;
